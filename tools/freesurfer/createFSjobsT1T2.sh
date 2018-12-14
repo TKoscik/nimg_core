b98746166f00b6f763c61fd974fddb9edc80306f
axelsone <32268049+axelsone@users.noreply.github.com>
2018-12-14 10:08:37 -0600

    Update createFSjobsT1T2.sh

--- tools/freesurfer/createFSjobsT1T2.sh
+++ tools/freesurfer/createFSjobsT1T2.sh
@@ -5,19 +5,18 @@ function Usage {
 
 `basename $0` creates freesurfer shell scripts to submit to cluster with appropriate template TMPL file in pwd 
 
-Usage:
 `basename $0`  -r researcher_directory
                         -p project_name
+                        -s site
                         -h <help>
-
+                        
 Example:
-  bash $0 -r /Shared/nopoulos -p sca_pilot
-
+  bash $0 -r /Shared/nopoulos -p sca_pilot -s 00201
 Arguments:
   -r researcher_directory  The full root directory the imaging project resides
   -p project_name          A unique name of the imaging project where the BFC T1s and T2s reside
+  -s site                  Input the site where scans were acquired 
   -h help
-
 USAGE
     exit 1
 }
@@ -32,6 +31,9 @@ in
     ;;
   p) # project_name
     projectName=${OPTARG}
+    ;;
+  s) # site
+    site=${OPTARG}
     ;;
   h) # help
     Usage >&2
@@ -52,7 +54,11 @@ for i in ${researcherRoot}/${projectName}/derivatives/anat/prep/[a-zA-Z0-9]*/[a-
  if [ "$T2bc" == "" ] ; then
    echo "Error:  No T2 exists for ${SUBJECTID}/${MRQID} use T1 only scripts"
  else 
- mkdir ${researcherRoot}/${projectName}/derivatives/anat/fsurf
+ if [ "${researcherRoot}/${projectName}/derivatives/anat/fsurf" == "" ] ; then
+   mkdir ${researcherRoot}/${projectName}/derivatives/anat/fsurf
+  else
+   echo "fsurf directory already exists"
+  fi
  SUBJECTID=$(echo $i |awk -F/ '{print $5}')
  MRQID=$(echo $i |awk -F/ '{print $6}')
  SHSCRIPTtmp=$(pwd)/${projectName}_FSwbcv6T1T2_${SUBJECTID}_${MRQID}_tmp.sh
